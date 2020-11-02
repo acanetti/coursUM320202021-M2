51287a270f3be7d7f79e39e714bd62228473baa4
natoine <antoineseilles@gmail.com>
2020-11-02 15:13:42 +0100

    now cors enabled

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ const port = process.env.PORT || 3000 ;
 var fetch = require('node-fetch');
 var https = require('https');
 
-app.get("/", function(req, res){
-    res.send("helloWorld !");
-})
+
+var cors = require('cors');
+
+var corsOptions = {
+    origin: 'https://natoine.github.io',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+//serves static files
+app.use(express.static('docs'));
+
+
+//ROUTES
 
 app.get("/:name", function(req, res){
     res.send("hello : " + req.params.name );
 })
 
-app.get("/fetchair/shangai", function(req, res){
+app.get("/fetchair/shangai", cors(corsOptions), function(req, res){
     let url = "http://api.waqi.info/feed/shanghai/?token=demo" ;
     fetch(url)
     .then(res => res.json())
