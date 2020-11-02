49ff3188accb6d8be100167b5d676f81069327aa
natoine <antoineseilles@gmail.com>
2020-11-02 14:16:53 +0100

    sample node fetch request

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ var app = express();
 
 const port = process.env.PORT || 3000 ;
 
+const fetch = require('node-fetch');
+
+
 app.get("/", function(req, res){
     res.send("helloWorld !");
+})
+
+app.get("/:name", function(req, res){
+    res.send("hello : " + req.params.name );
+})
+
+app.get("/fetchair/shangai", function(req, res){
+    let url = "http://api.waqi.info/feed/shanghai/?token=demo" ;
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json);
+        res.send("data fetched look your console");
+    });
 })
 
 app.listen(port, function () {
